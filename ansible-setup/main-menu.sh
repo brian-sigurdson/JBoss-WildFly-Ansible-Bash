29a50b7ca5fb71921c5753cb3f33e9459e24733c
Brian Sigurdson <sigurdson.brian@gmail.com>
2018-10-29 17:04:36 -0400

    Updates

--- ansible-setup/main-menu.sh
+++ ansible-setup/main-menu.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-####################################################################################################################
-# Assumptions
-# 1)  This script is run as sudo on the master node.  
-# 2)  The script assumes that slave host names are in a file named slaves.txt and are resolveable
-# 3)  ** Please see additional notes and assumptions at the top of ansible-setup.sh
-
-# Purpose
-# The purpose of these scripts are to prepare the master and slave nodes to use Ansible for cluster management.
-# 
-# Ansible is installed on the master, but only Python 2.7 is necessary on the slave nodes, according to
-# 	http://docs.ansible.com/ansible/latest/intro_installation.html#basics-what-will-be-installed
-#
-# A user "ansible" is installed on all nodes with sudo privileges without password prompt.  This and the use of ssh
-#	keys to allow passwordless login, allow cluser management from the master node.
-####################################################################################################################
-
 # globals
 ANSIBLE_UN="ansible"
 ANSIBLE_PWD="$ANSIBLE_UN"
-SLAVE_FILE="slaves.txt"
+SLAVE_FILE="node.txt"
 SHOW_EXPECT_SCRIPT_MSG=1
+NODE_SETUP_LOG=files_output/node-setup.log
 
 ####################################################################################################################
 # menu for Ansible setup on the master and slave nodes
@@ -33,11 +18,10 @@ while [[ $REPLY != 0 ]]; do
 	echo "-----------------------------------"
 	echo "           Ansible Setup"
 	echo "-----------------------------------"
-	echo "1) Master Setup"
-	echo "2) Slaves Setup (all)"
+	echo "1) Node Setup"
 	echo "0) Quit / Exit"
 	echo "-----------------------------------"
-	read -p "Enter Selection [0-2]: "
+	read -p "Enter Selection [0-1]: "
 	echo "-----------------------------------"
 	echo ""
 
@@ -49,33 +33,14 @@ while [[ $REPLY != 0 ]]; do
 			;;
 
 		1) 	echo ""
-			echo "1) Master Setup Selected."
+			echo "1) Node Setup Selected."
 
-			echo "   output to:  `pwd`/files_output/master-setup.log"
+			echo "   output to:  `pwd`/$NODE_SETUP_LOG"
 			sleep 5
 
 			echo ""
-			./ansible-setup.sh $ANSIBLE_UN $ANSIBLE_PWD $REPLY $SLAVE_FILE | tee `pwd`/files_output/master-setup.log
-			chown `logname`.`logname` `pwd`/files_output/master-setup.log
-			echo ""
-			
-			read -n 1 -s -r -p "Press any key to continue"
-			;;
-
-		2) 	echo ""
-			echo "2) Slave Setup Selected."
-		
-			cat <<- _EOF_
-
-			Note:	
-			Managed node (slaves) log files will be stored in 
-			`pwd`/files_output upon upon completion of updates.
-			_EOF_
-			sleep 5
-
-			echo ""
-			./ansible-setup.sh $ANSIBLE_UN $ANSIBLE_PWD $REPLY $SLAVE_FILE $SHOW_EXPECT_SCRIPT_MSG
-			chown -R `logname`.`logname` `pwd`/files_output/
+			./ansible-setup.sh $ANSIBLE_UN $ANSIBLE_PWD $REPLY $SLAVE_FILE | tee `pwd`/$NODE_SETUP_LOG
+			chown `logname`.`logname` `pwd`/$NODE_SETUP_LOG
 			echo ""
 			
 			read -n 1 -s -r -p "Press any key to continue"
